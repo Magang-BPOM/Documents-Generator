1866f55e780deee572ea41a9082b8c98de43c6b1
Haikalasad <muhammad.haikal.21@student.if.ittelkom-sby.ac.id>
2024-10-24 14:21:17 +0700

    Feat : Add login feature

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
 $routes->get('/dashboard', 'Dashboard::index');
 
+
+$routes->get('/', 'User::index');
+$routes->post('login', 'User::login');
+$routes->get('logout', 'User::logout');
---
+++ app/Controllers/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\User as ModelsUser;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class User extends BaseController
+{
+    public function index()
+    {
+        return view('auth/login');
+    }
+
+    public function login()
+{
+    $session = session();
+    $validation = \Config\Services::validation();
+
+    $identifier = $this->request->getVar('identifier');
+    $password = $this->request->getVar('password');
+
+    $validation->setRules([
+        'identifier' => 'required',
+        'password' => 'required',
+    ]);
+
+    if (!$validation->withRequest($this->request)->run()) {
+        return redirect()->back()->with('errors', $validation->getErrors())->withInput();
+    }
+
+    $userModel = new ModelsUser(); 
+    $user = $userModel->where('nip', $identifier)->first();
+
+    if ($user && password_verify($password, $user['password'])) {
+    
+        $session->set([
+            'user_id' => $user['id'],
+            'username' => $user['nama'], 
+            'is_logged_in' => true,
+        ]);
+
+        return redirect()->to('/dashboard');
+    } else {
+        $session->setFlashdata('status', 'NIP atau Password salah.');
+        return redirect()->back()->withInput();
+    }
+}
+
+    
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('/login');
+    }
+}
