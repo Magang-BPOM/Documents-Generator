f60c586f218ee47969bf334e8d0494c71218d5c9
tegararta <tegararta@gmail.com>
2024-10-24 16:07:22 +0700

    Dokumne

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/dashboard', 'Dashboard::index');
 
+$routes->get('/dok', 'DokumenController::index');
+$routes->get('/dokumen/create', 'DokumenController::create');
+$routes->post('/dokumen/submit', 'DokumenController::submit');
